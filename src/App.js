47333b63e603a8a974b3ccd4c3f6b6672237f5a7
Kim._.jazz <jeong011010@naver.com>
2022-03-03 14:13:21 +0900

    update js file [start to make todo list ]

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import TodoList from './TodoList';
 
 function App() {
+
+  const [lists,setlists] = useState('')
+  const [data,setdata] = useState('');
+  //const todolist = lists.map((list)=> (<li className="item">{list}</li>));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="todolist">
+        <div className="main_title">
+          <h1>To do list</h1>
+        </div>
+        <div>
+          <form className="input_section">
+            <div>
+              <input type="text" className="input_item" onChange={event=>setdata(event.target.value)}/>
+              <button className="input_button" onClick={()=>{setdata('');}}>+</button>
+              <TodoList/>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/TodoList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TodoList(){
+    return(
+        <li>!</li>
+    )
+}
+
+export default TodoList
