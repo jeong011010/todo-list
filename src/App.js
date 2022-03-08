1a781318a6db94991c5329891baae2c9a9553e15
Kim._.jazz <jeong011010@naver.com>
2022-03-08 16:13:12 +0900

    update js file [update check function]

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import './App.css';
 
 function App() {
 
-  let [lists, setlists] = useState([])
+  const [lists, setlists] = useState([]);
   const [data,setdata] = useState('')
-  const [tmp,settmp]=useState(0)
+  const [tmp,settmp]=useState(false)
+  const [id,setid] = useState(0)
   const [darkMode, setDarkMode] = useState(false)
-  const [check, setCheck] = useState(false)
 
   const todolist = lists.map((list)=> (
 
       list.deleted === false
-      ?<div className="items"  onClick={()=>{setCheck(!check);}} style={{backgroundColor: darkMode?'black':'rgb(230, 230, 230)',
+      ?<div className="items" style={{backgroundColor: darkMode?'black':'rgb(230, 230, 230)',
       color: darkMode?'lightgray':'black'}}
       >
-        <div className="item" 
-        style={{textDecoration: check?'line-through':'none'}}>{list.data}</div>
-        <span onClick={()=>{list.deleted=true; settmp(tmp+1)}}>🗑️</span>
+        <div className="item" style={{textDecoration: list.checked?'line-through':'none'}}>
+          {list.data}</div>
+        <div>
+          <span onClick={()=>{list.checked=(!list.checked); settmp(!tmp);}}>{list.checked?'✅':'🟩'}</span>
+          <span onClick={()=>{list.deleted=true; settmp(!tmp);}}>🗑️</span>
+        </div>
       </div>
       : null
 
@@ -44,7 +47,7 @@ function App() {
               <div>
                 <input className="input_item" onChange={event=>setdata(event.target.value)}
                 style={{backgroundColor: darkMode?'gray':'white',color: darkMode?'lightgray':'black'}}/>
-                <button className="input_button" onClick={ () => {setlists([...lists,{data:data,deleted:false}])}}
+                <button className="input_button" onClick={ () => {setlists([...lists,{id:id,data:data,deleted:false,checked:false,}]); setid(id+1)}}
                 style={{color: darkMode?'black':'white',backgroundColor: darkMode?'gray':'#ade2cc'}}>+</button>
               </div>
           </div>
